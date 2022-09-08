069c257bd20aa18fd5e54a440136fac065a5afa9
Camilo Fernandez <camiloferdez@hotmail.com>
2022-09-07 23:27:16 -0300

    agregue el loading

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -2,27 +2,30 @@ import { useState, useEffect } from 'react'
 import './ItemDetailContainer.css'
 import { useParams } from 'react-router-dom'
 import { ItemDetail } from '../ItemDetail/ItemDetail'
-
+import { Loading } from '../Loading/Loading'
 
 export const ItemDetailContainer = () => {
   const { id } = useParams()
 
   const [details, setDetails] = useState({})
-
+  const [loading, setLoading] = useState(true)
   const getBook = async () => {
     const booksJson = await fetch('../../json/Books.json')
     const booksArray = await booksJson.json()
     const bookFind = await booksArray.find((book => 
-      { return book.id == Number(id);
+      { return book.id === Number(id);
       }))
     setDetails(bookFind)
+    setLoading(false)
   }
 
   useEffect(() => {
-    getBook()
+    setTimeout(() => {
+      getBook()
+    }, 1000);
   }, [])
 
   return (
-    <ItemDetail name={details.nombre} autor={details.autor} stock={details.stock} sinopsis={details.sinopsis} img={details.img} editorial={details.editorial} price={details.precio}/>
+    loading ? <Loading/> : <ItemDetail name={details.nombre} autor={details.autor} stock={details.stock} sinopsis={details.sinopsis} img={details.img} editorial={details.editorial} price={details.precio}/>
   )
 }
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,26 +1,31 @@
 import { useState, useEffect } from 'react'
 import './ItemListContainer.css'
 import { ItemList } from '../ItemList/ItemList'
-
+import { Loading } from '../Loading/Loading'
 
 export const ItemListContainer = (props) => {
 
   const [books, setBooks] = useState([])
+  const [loading, setLoading] = useState(true)
 
   const getBooks = async ()=>{
     const booksJson = await fetch('./json/Books.json')
     const booksArray = await booksJson.json()
     setBooks(booksArray)
+    setLoading(false)
   }
 
+
   useEffect(() => {
-    getBooks()
+    setTimeout(() => {
+      getBooks()
+    }, 1000);
   }, [])
   
 
   return (
-    <main className='item-list-container'>
-      <ItemList books={books}/>
+    loading ? <Loading/> : <main className='item-list-container'>
+     <ItemList books={books}/>
     </main>
   )
 }
